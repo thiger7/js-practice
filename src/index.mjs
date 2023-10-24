d56cccafa63e1aa7bd0b631eeb3d924da5fc82b8
えいむん <103919186+thiger7@users.noreply.github.com>
2023-10-24 22:27:38 +0900

    Initial commit

---
+++ src/index.mjs
@@ -0,0 +1,40 @@
+/**
+ * const, let等の変数宣言
+ */
+
+// var val1 = "var変数";
+// console.log(val1);
+
+// // var変数は上書き可能
+// val1 = "val変数を上書き";
+// console.log(val1);
+
+// 　// var変数は再宣言可能
+// var val1 = "var変数を再宣言";
+// console.log(val1);
+
+// let val2 = "let変数";
+// console.log(val2);
+
+// val2 = "let変数を上書き"
+// console.log(val2)
+
+// // letは再宣言不可能
+// let val2 = "let変数を再宣言"
+
+// const val3 = "const変数";
+// console.log(val3);
+
+// const変数は上書き不可
+// val3 = "const変数を上書き"
+
+// const val3 = "const変数を再宣言"
+
+// constで定義したオブジェクトはプロパティの変更が可能
+// const val4 = {
+//   name: "テスト",
+//   age: 32,
+// };
+// val4.name = "test";
+// val4.address = "Ishikawa"
+// console.log(val4);
